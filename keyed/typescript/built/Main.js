8c253663854285b82f3e0724e5cfa16f8630d19a
nienaber <rene-sahin.nienaber@adesso.de>
2023-10-16 16:32:57 +0200

    translate js to ts with strict tsconfig

--- keyed/typescript/built/Main.js
+++ keyed/typescript/built/Main.js
@@ -1,18 +1,16 @@
-'use strict';
 function _random(max) {
     return Math.round(Math.random() * 1000) % max;
 }
-var rowTemplate = document.createElement("tr");
+const rowTemplate = document.createElement("tr");
 rowTemplate.innerHTML = "<td class='col-md-1'></td><td class='col-md-4'><a class='lbl'></a></td><td class='col-md-1'><a class='remove'><span class='remove glyphicon glyphicon-remove' aria-hidden='true'></span></a></td><td class='col-md-6'></td>";
-var Store = /** @class */ (function () {
-    function Store() {
+class Store {
+    constructor() {
         this.data = [];
         this.backup = null;
         this.selected = null;
         this.id = 1;
     }
-    Store.prototype.buildData = function (count) {
-        if (count === void 0) { count = 1000; }
+    buildData(count = 1000) {
         var adjectives = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
         var colours = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
         var nouns = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
@@ -20,61 +18,60 @@ var Store = /** @class */ (function () {
         for (var i = 0; i < count; i++)
             data.push({ id: this.id++, label: adjectives[_random(adjectives.length)] + " " + colours[_random(colours.length)] + " " + nouns[_random(nouns.length)] });
         return data;
-    };
-    Store.prototype.updateData = function (mod) {
-        if (mod === void 0) { mod = 10; }
-        for (var i = 0; i < this.data.length; i += 10) {
+    }
+    updateData(mod = 10) {
+        for (let i = 0; i < this.data.length; i += 10) {
             this.data[i].label += ' !!!';
-            // this.data[i] = Object.assign({}, this.data[i], {label: this.data[i].label +' !!!'});
         }
-    };
-    Store.prototype.delete = function (id) {
-        var idx = this.data.findIndex(function (d) { return d.id == id; });
-        this.data = this.data.filter(function (e, i) { return i != idx; });
+    }
+    delete(id) {
+        const idx = this.data.findIndex(d => d.id == id);
+        this.data = this.data.filter((e, i) => i != idx);
         return this;
-    };
-    Store.prototype.run = function () {
+    }
+    run() {
         this.data = this.buildData();
         this.selected = null;
-    };
-    Store.prototype.add = function () {
+    }
+    add() {
         this.data = this.data.concat(this.buildData(1000));
         this.selected = null;
-    };
-    Store.prototype.update = function () {
+    }
+    update() {
         this.updateData();
         this.selected = null;
-    };
-    Store.prototype.select = function (id) {
+    }
+    select(id) {
         this.selected = id;
-    };
-    Store.prototype.hideAll = function () {
+    }
+    hideAll() {
         this.backup = this.data;
         this.data = [];
         this.selected = null;
-    };
-    Store.prototype.showAll = function () {
-        this.data = this.backup;
+    }
+    showAll() {
+        if (this.backup) {
+            this.data = this.backup;
+        }
         this.backup = null;
         this.selected = null;
-    };
-    Store.prototype.runLots = function () {
+    }
+    runLots() {
         this.data = this.buildData(10000);
         this.selected = null;
-    };
-    Store.prototype.clear = function () {
+    }
+    clear() {
         this.data = [];
         this.selected = null;
-    };
-    Store.prototype.swapRows = function () {
+    }
+    swapRows() {
         if (this.data.length > 998) {
             var a = this.data[1];
             this.data[1] = this.data[998];
             this.data[998] = a;
         }
-    };
-    return Store;
-}());
+    }
+}
 var getParentId = function (elem) {
     while (elem) {
         if (elem.tagName === "TR") {
@@ -84,9 +81,9 @@ var getParentId = function (elem) {
     }
     return undefined;
 };
-var Main = /** @class */ (function () {
-    function Main(props) {
-        var _this = this;
+class Main {
+    constructor() {
+        var _a;
         this.store = new Store();
         this.select = this.select.bind(this);
         this.delete = this.delete.bind(this);
@@ -97,73 +94,58 @@ var Main = /** @class */ (function () {
         this.rows = [];
         this.data = [];
         this.selectedRow = undefined;
-        document.getElementById("main").addEventListener('click', function (e) {
-            //console.log("listener",e);
+        (_a = document.getElementById("main")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', e => {
             if (e.target.matches('#add')) {
                 e.preventDefault();
-                //console.log("add");
-                _this.add();
+                this.add();
             }
             else if (e.target.matches('#run')) {
                 e.preventDefault();
-                //console.log("run");
-                _this.run();
+                this.run();
             }
             else if (e.target.matches('#update')) {
                 e.preventDefault();
-                //console.log("update");
-                _this.update();
-            }
-            else if (e.target.matches('#hideall')) {
-                e.preventDefault();
-                //console.log("hideAll");
-                _this.hideAll();
-            }
-            else if (e.target.matches('#showall')) {
-                e.preventDefault();
-                //console.log("showAll");
-                _this.showAll();
+                this.update();
             }
             else if (e.target.matches('#runlots')) {
                 e.preventDefault();
-                //console.log("runLots");
-                _this.runLots();
+                this.runLots();
             }
             else if (e.target.matches('#clear')) {
                 e.preventDefault();
-                //console.log("clear");
-                _this.clear();
+                this.clear();
             }
             else if (e.target.matches('#swaprows')) {
                 e.preventDefault();
-                //console.log("swapRows");
-                _this.swapRows();
+                this.swapRows();
             }
             else if (e.target.matches('.remove')) {
                 e.preventDefault();
-                var id = getParentId(e.target);
-                var idx = _this.findIdx(id);
-                //console.log("delete",idx);
-                _this.delete(idx);
+                let id = getParentId(e.target);
+                let idx = this.findIdx(id);
+                if (idx) {
+                    this.delete(idx);
+                }
             }
             else if (e.target.matches('.lbl')) {
                 e.preventDefault();
-                var id = getParentId(e.target);
-                var idx = _this.findIdx(id);
-                //console.log("select",idx);
-                _this.select(idx);
+                let id = getParentId(e.target);
+                let idx = this.findIdx(id);
+                if (idx) {
+                    this.select(idx);
+                }
             }
         });
         this.tbody = document.getElementById("tbody");
     }
-    Main.prototype.findIdx = function (id) {
-        for (var i = 0; i < this.data.length; i++) {
+    findIdx(id) {
+        for (let i = 0; i < this.data.length; i++) {
             if (this.data[i].id === id)
                 return i;
         }
         return undefined;
-    };
-    Main.prototype.run = function () {
+    }
+    run() {
         this.removeAllRows();
         this.store.clear();
         this.rows = [];
@@ -171,70 +153,52 @@ var Main = /** @class */ (function () {
         this.store.run();
         this.appendRows();
         this.unselect();
-    };
-    Main.prototype.add = function () {
+    }
+    add() {
         this.store.add();
         this.appendRows();
-    };
-    Main.prototype.update = function () {
+    }
+    update() {
         this.store.update();
-        for (var i = 0; i < this.data.length; i += 10) {
+        for (let i = 0; i < this.data.length; i += 10) {
             this.rows[i].childNodes[1].childNodes[0].innerText = this.store.data[i].label;
         }
-    };
-    Main.prototype.unselect = function () {
+    }
+    unselect() {
         if (this.selectedRow !== undefined) {
             this.selectedRow.className = "";
             this.selectedRow = undefined;
         }
-    };
-    Main.prototype.select = function (idx) {
+    }
+    select(idx) {
         this.unselect();
         this.store.select(this.data[idx].id);
         this.selectedRow = this.rows[idx];
         this.selectedRow.className = "danger";
-    };
-    Main.prototype.recreateSelection = function () {
-        var old_selection = this.store.selected;
-        var sel_idx = this.store.data.findIndex(function (d) { return d.id === old_selection; });
+    }
+    recreateSelection() {
+        let old_selection = this.store.selected;
+        let sel_idx = this.store.data.findIndex(d => d.id === old_selection);
         if (sel_idx >= 0) {
             this.store.select(this.data[sel_idx].id);
             this.selectedRow = this.rows[sel_idx];
             this.selectedRow.className = "danger";
         }
-    };
-    Main.prototype.delete = function (idx) {
-        // Remove that row from the DOM
+    }
+    delete(idx) {
         this.store.delete(this.data[idx].id);
         this.rows[idx].remove();
         this.rows.splice(idx, 1);
         this.data.splice(idx, 1);
         this.unselect();
         this.recreateSelection();
-    };
-    Main.prototype.removeAllRows = function () {
-        // ~258 msecs
-        // for(let i=this.rows.length-1;i>=0;i--) {
-        //     tbody.removeChild(this.rows[i]);
-        // }
-        // ~251 msecs
-        // for(let i=0;i<this.rows.length;i++) {
-        //     tbody.removeChild(this.rows[i]);
-        // }
-        // ~216 msecs
-        // var cNode = tbody.cloneNode(false);
-        // tbody.parentNode.replaceChild(cNode ,tbody);
-        // ~212 msecs
-        this.tbody.textContent = "";
-        // ~236 msecs
-        // var rangeObj = new Range();
-        // rangeObj.selectNodeContents(tbody);
-        // rangeObj.deleteContents();
-        // ~260 msecs
-        // var last;
-        // while (last = tbody.lastChild) tbody.removeChild(last);
-    };
-    Main.prototype.runLots = function () {
+    }
+    removeAllRows() {
+        if (this.tbody) {
+            this.tbody.textContent = "";
+        }
+    }
+    runLots() {
         this.removeAllRows();
         this.store.clear();
         this.rows = [];
@@ -242,67 +206,43 @@ var Main = /** @class */ (function () {
         this.store.runLots();
         this.appendRows();
         this.unselect();
-    };
-    Main.prototype.clear = function () {
+    }
+    clear() {
         this.store.clear();
         this.rows = [];
         this.data = [];
-        // This is actually a bit faster, but close to cheating
-        // requestAnimationFrame(() => {
         this.removeAllRows();
         this.unselect();
-        // });
-    };
-    Main.prototype.swapRows = function () {
+    }
+    swapRows() {
+        var _a, _b;
         if (this.data.length > 10) {
             this.store.swapRows();
             this.data[1] = this.store.data[1];
             this.data[998] = this.store.data[998];
-            this.tbody.insertBefore(this.rows[998], this.rows[2]);
-            this.tbody.insertBefore(this.rows[1], this.rows[999]);
-            var tmp = this.rows[998];
+            (_a = this.tbody) === null || _a === void 0 ? void 0 : _a.insertBefore(this.rows[998], this.rows[2]);
+            (_b = this.tbody) === null || _b === void 0 ? void 0 : _b.insertBefore(this.rows[1], this.rows[999]);
+            let tmp = this.rows[998];
             this.rows[998] = this.rows[1];
             this.rows[1] = tmp;
         }
-        // let old_selection = this.store.selected;
-        // this.store.swapRows();
-        // this.updateRows();
-        // this.unselect();
-        // if (old_selection>=0) {
-        //     let idx = this.store.data.findIndex(d => d.id === old_selection);
-        //     if (idx > 0) {
-        //         this.store.select(this.data[idx].id);
-        //         this.selectedRow = this.rows[idx];
-        //         this.selectedRow.className = "danger";
-        //     }
-        // }
-    };
-    Main.prototype.appendRows = function () {
-        // Using a document fragment is slower...
-        // var docfrag = document.createDocumentFragment();
-        // for(let i=this.rows.length;i<this.store.data.length; i++) {
-        //     let tr = this.createRow(this.store.data[i]);
-        //     this.rows[i] = tr;
-        //     this.data[i] = this.store.data[i];
-        //     docfrag.appendChild(tr);
-        // }
-        // this.tbody.appendChild(docfrag);
-        // ... than adding directly
+    }
+    appendRows() {
         var rows = this.rows, s_data = this.store.data, data = this.data, tbody = this.tbody;
-        for (var i = rows.length; i < s_data.length; i++) {
-            var tr = this.createRow(s_data[i]);
+        for (let i = rows.length; i < s_data.length; i++) {
+            let tr = this.createRow(s_data[i]);
             rows[i] = tr;
             data[i] = s_data[i];
-            tbody.appendChild(tr);
+            tbody === null || tbody === void 0 ? void 0 : tbody.appendChild(tr);
         }
-    };
-    Main.prototype.createRow = function (data) {
-        var tr = rowTemplate.cloneNode(true), td1 = tr.firstChild, a2 = td1.nextSibling.firstChild;
+    }
+    createRow(data) {
+        var _a;
+        const tr = rowTemplate.cloneNode(true), td1 = tr.firstChild, a2 = (_a = td1 === null || td1 === void 0 ? void 0 : td1.nextSibling) === null || _a === void 0 ? void 0 : _a.firstChild;
         tr.data_id = data.id;
-        td1.textContent = data.id;
+        td1.textContent = data.id.toString();
         a2.textContent = data.label;
         return tr;
-    };
-    return Main;
-}());
+    }
+}
 new Main();
