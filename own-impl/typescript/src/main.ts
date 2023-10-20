bdc910f071b873269f7247733a82100d75ef579e
nienaber <rene-sahin.nienaber@adesso.de>
2023-10-20 17:05:49 +0200

    set up own typescript implementation of benchmark

---
+++ own-impl/typescript/src/main.ts
@@ -0,0 +1,23 @@
+function run() {
+
+}
+
+function runLots(): void {
+
+}
+
+function add(): void {
+
+}
+
+function update(): void {
+
+}
+
+function clear(): void {
+
+}
+
+function swapRows(): void {
+
+}
