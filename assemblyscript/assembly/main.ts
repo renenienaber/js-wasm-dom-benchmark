239656a865c01abb07f5ea61b6fb5fab6d42e4e2
nienaber <rene-sahin.nienaber@adesso.de>
2023-10-21 14:42:13 +0200

    add neccessary modifications for compiling ideomatic logic to assemblyscript to webassembly

--- assemblyscript/assembly/main.ts
+++ assemblyscript/assembly/main.ts
@@ -65,8 +65,10 @@
 
 // setup
 
-interface RowElement {
+class RowElement {
+    // @ts-ignore
     id: number;
+    // @ts-ignore
     label: string;
 }
 
@@ -74,15 +76,19 @@ export function buildData(count: number = 1000, firstId: number = 1): RowElement
     const adjectives: string[] = ["pretty", "large", "big", "small", "tall", "short", "long", "handsome", "plain", "quaint", "clean", "elegant", "easy", "angry", "crazy", "helpful", "mushy", "odd", "unsightly", "adorable", "important", "inexpensive", "cheap", "expensive", "fancy"];
     const colours: string[] = ["red", "yellow", "blue", "green", "pink", "brown", "purple", "brown", "white", "black", "orange"];
     const nouns: string[] = ["table", "chair", "house", "bbq", "desk", "car", "pony", "cookie", "sandwich", "burger", "pizza", "mouse", "keyboard"];
-    const data: RowElement[] = [];
-    for (let i = 0; i < count; i++)
-        data.push({id: firstId+i, label: adjectives[_random(adjectives.length)] + " " + colours[_random(colours.length)] + " " + nouns[_random(nouns.length)] });
+    const data: RowElement[] = new Array<RowElement>(<i32>count);
+    for (let i = 0; i < count; i++) {
+        data[i] = {
+            id: firstId + i,
+            label: adjectives[<i32>_random(adjectives.length)] + " " + colours[<i32>_random(colours.length)] + " " + nouns[<i32>_random(nouns.length)]
+        }
+    }
     return data;
 }
 
 export function updateData(rowElements: RowElement[], mod: number = 10): RowElement[] {
     const updatedElements = rowElements;
-    for (let i=0; i < updatedElements.length; i+=mod) {
+    for (let i=0; i < updatedElements.length; i += <i32>mod) {
         updatedElements[i].label += ' !!!';
     }
     return updatedElements;
